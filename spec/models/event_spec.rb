b8baa45d7a2e34cdddce9d043df9406c4ebecd7f
Isa <pdo.isabelle@gmail.com>
2023-09-26 18:18:52 -0300

    Inserção de testes referentes a Event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '#organizer_event' do
+		it 'organizes lectures on tracks' do
+			event = Event.create(name: 'Meu Evento')
+			lecture1 = Lecture.create(name: 'Palestra 1', allocated: false, event: event, lecture_minutes: 30)
+			lecture2 = Lecture.create(name: 'Palestra 2', allocated: false, event: event, lecture_minutes: 45)
+
+			expect do
+				event.organizer_event
+			end.to change { event.tracks.count }.by(1)
+
+			expect(lecture1.reload.allocated).to be true
+			expect(lecture2.reload.allocated).to be true
+		end
+	end
 end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,10 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe "Events", type: :request do
-  describe "GET /events" do
-    it "works! (now write some real specs)" do
-      get events_path
-      expect(response).to have_http_status(200)
-    end
-  end
+RSpec.describe 'Events API', type: :request do
+	describe 'GET /events' do
+		it 'returns a list of events' do
+			create_list(:event, 3)
+
+			get '/events'
+
+			expect(response).to have_http_status(200)
+			expect(JSON.parse(response.body).size).to eq(3)
+		end
+	end
+
+	describe 'POST /events' do
+		it 'create a new event' do
+			event_params = {
+				name: 'Evento de Teste',
+				date: '2023-10-01',
+				location: 'Local de Teste'
+			}
+
+			expect do
+				post '/events', params: { event: event_params }
+			end.to change(Event, :count).by(1)
+
+			expect(response).to have_http_status(201)
+		end
+	end
+
+	describe 'GET /events/:id' do
+		it 'returns an event' do
+			event = create(:event)
+
+			get "/events/#{event.id}"
+
+			expect(response).to have_http_status(200)
+			expect(JSON.parse(response.body)['name']).to eq(event.name)
+		end
+	end
+
+	describe 'PUT /events/:id' do
+		it 'update an event' do
+			event = create(:event)
+			updated_attributes = { name: 'Novo Nome' }
+
+			put "/events/#{event.id}", params: { event: updated_attributes }
+
+			expect(response).to have_http_status(204)
+			expect(event.reload.name).to eq('Novo Nome')
+		end
+	end
+
+	describe 'DELETE /events/:id' do
+		it 'delete an event' do
+			event = create(:event)
+
+			expect do
+				delete "/events/#{event.id}"
+			end.to change(Event, :count).by(-1)
+
+			expect(response).to have_http_status(204)
+		end
+	end
 end
+
